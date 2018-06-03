b50429c1abe9a6d578e6ab741052268bc48466d4
Yuan Cao <caoyuan0816@gmail.com>
2018-06-04 02:51:50 +0800

    107

---
+++ 107.binary-tree-level-order-traversal-ii.python3.py
@@ -0,0 +1,62 @@
+#
+# [107] Binary Tree Level Order Traversal II
+#
+# https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+#
+# algorithms
+# Easy (42.63%)
+# Total Accepted:    168.2K
+# Total Submissions: 394.3K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the bottom-up level order traversal of its nodes'
+# values. (ie, from left to right, level by level from leaf to root).
+#
+#
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+#
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+#
+#
+#
+# return its bottom-up level order traversal as:
+#
+# [
+# ⁠ [15,7],
+# ⁠ [9,20],
+# ⁠ [3]
+# ]
+#
+#
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        self.res = []
+        self.DFS(root, 0)
+        return self.res[::-1]
+
+    def DFS(self, root, level):
+        if root is None:
+            return
+        if level >= len(self.res):
+            self.res.append([])
+        self.res[level].append(root.val)
+        self.DFS(root.left, level+1)
+        self.DFS(root.right, level+1)
