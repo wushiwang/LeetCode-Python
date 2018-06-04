e5dfcdfe0a7b891ee76eeb524864c86e7b26e87e
Yuan Cao <caoyuan0816@gmail.com>
2018-06-05 01:02:48 +0800

    120

---
+++ 120.triangle.python3.py
@@ -0,0 +1,52 @@
+#
+# [120] Triangle
+#
+# https://leetcode.com/problems/triangle/description/
+#
+# algorithms
+# Medium (35.34%)
+# Total Accepted:    135.8K
+# Total Submissions: 384.2K
+# Testcase Example:  '[[2],[3,4],[6,5,7],[4,1,8,3]]'
+#
+# Given a triangle, find the minimum path sum from top to bottom. Each step you
+# may move to adjacent numbers on the row below.
+#
+# For example, given the following triangle
+#
+#
+# [
+# ⁠    [2],
+# ⁠   [3,4],
+# ⁠  [6,5,7],
+# ⁠ [4,1,8,3]
+# ]
+#
+#
+# The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+#
+# Note:
+#
+# Bonus point if you are able to do this using only O(n) extra space, where n
+# is the total number of rows in the triangle.
+#
+
+
+class Solution:
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    triangle[i][j] += triangle[i-1][j]
+                elif j == len(triangle[i])-1:
+                    triangle[i][j] += triangle[i-1][j-1]
+                else:
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
+        res = triangle[-1][0]
+        for it in triangle[-1]:
+            res = min(res, it)
+        return res
