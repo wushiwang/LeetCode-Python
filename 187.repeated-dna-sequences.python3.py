99fec885f01df4b248c96b9e01eab20b004cf686
Yuan Cao <caoyuan0816@gmail.com>
2018-06-15 23:13:09 +0800

    187

---
+++ 187.repeated-dna-sequences.python3.py
@@ -0,0 +1,53 @@
+#
+# [187] Repeated DNA Sequences
+#
+# https://leetcode.com/problems/repeated-dna-sequences/description/
+#
+# algorithms
+# Medium (33.36%)
+# Total Accepted:    97.5K
+# Total Submissions: 292.1K
+# Testcase Example:  '"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"'
+#
+# All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T,
+# for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to
+# identify repeated sequences within the DNA.
+#
+# Write a function to find all the 10-letter-long sequences (substrings) that
+# occur more than once in a DNA molecule.
+#
+# Example:
+#
+#
+# Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+#
+# Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+#
+
+
+class Solution:
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s) < 10:
+            return []
+        use = {'A': 1, 'C': 2, 'G': 3, 'T':4}
+        dic, res = dict(), []
+        cur, L, R = 0, 0, 0
+        while R != 9:
+            cur *= 10
+            cur += use[s[R]]
+            R += 1
+        while R < len(s):
+            cur *= 10
+            cur += use[s[R]]
+            if cur not in dic:
+                dic[cur] = 1
+            elif dic[cur] == 1:
+                dic[cur] += 1
+                res.append(s[L:R+1])
+            cur -= use[s[L]]*1000000000
+            L, R = L+1, R+1
+        return res
