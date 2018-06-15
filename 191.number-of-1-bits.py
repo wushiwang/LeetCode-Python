419d40b33ea6f156c760e6051434baebdd8c46d4
Yuan Cao <caoyuan0816@gmail.com>
2018-06-16 07:47:38 +0800

    191 Lowbit

--- 191.number-of-1-bits.py
+++ 191.number-of-1-bits.py
@@ -39,6 +39,6 @@ class Solution(object):
         """
         res = 0
         while n != 0:
-            res += (n&1)
-            n >>= 1
+            res += 1
+            n &= (n-1)
         return res
