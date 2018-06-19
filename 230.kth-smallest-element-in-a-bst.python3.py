a53307f892f502e43bda474794dc77618b4ca9e6
Yuan Cao <caoyuan0816@gmail.com>
2018-06-19 17:34:58 +0800

    230

---
+++ 230.kth-smallest-element-in-a-bst.python3.py
@@ -0,0 +1,77 @@
+#
+# [230] Kth Smallest Element in a BST
+#
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+#
+# algorithms
+# Medium (45.95%)
+# Total Accepted:    148.7K
+# Total Submissions: 323.3K
+# Testcase Example:  '[3,1,4,null,2]\n1'
+#
+# Given a binary search tree, write a function kthSmallest to find the kth
+# smallest element in it.
+#
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+#
+# Example 1:
+#
+#
+# Input: root = [3,1,4,null,2], k = 1
+# ⁠  3
+# ⁠ / \
+# ⁠1   4
+# ⁠ \
+# 2
+# Output: 1
+#
+# Example 2:
+#
+#
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# ⁠      5
+# ⁠     / \
+# ⁠    3   6
+# ⁠   / \
+# ⁠  2   4
+# ⁠ /
+# ⁠1
+# Output: 3
+#
+#
+# Follow up:
+# What if the BST is modified (insert/delete operations) often and you need to
+# find the kth smallest frequently? How would you optimize the kthSmallest
+# routine?
+#
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        stack.append(root)
+        cnt = 0
+        while len(stack) != 0:
+            cur = stack.pop()
+            if cur is not None:
+                if type(cur) is TreeNode:
+                    stack.append(cur.right)
+                    stack.append(cur.val)
+                    stack.append(cur.left)
+                else:
+                    cnt += 1
+                    if cnt == k:
+                        return cur
