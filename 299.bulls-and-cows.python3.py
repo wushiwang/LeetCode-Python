443f6fd349f7633131bd0947e5c46ec172c814ae
Yuan Cao <caoyuan0816@gmail.com>
2018-06-26 16:51:27 +0800

    299

---
+++ 299.bulls-and-cows.python3.py
@@ -0,0 +1,76 @@
+#
+# [299] Bulls and Cows
+#
+# https://leetcode.com/problems/bulls-and-cows/description/
+#
+# algorithms
+# Medium (36.31%)
+# Total Accepted:    69.9K
+# Total Submissions: 192.6K
+# Testcase Example:  '"1807"\n"7810"'
+#
+# You are playing the following Bulls and Cows game with your friend: You write
+# down a number and ask your friend to guess what the number is. Each time your
+# friend makes a guess, you provide a hint that indicates how many digits in
+# said guess match your secret number exactly in both digit and position
+# (called "bulls") and how many digits match the secret number but locate in
+# the wrong position (called "cows"). Your friend will use successive guesses
+# and hints to eventually derive the secret number.
+#
+# Write a function to return a hint according to the secret number and friend's
+# guess, use A to indicate the bulls and B to indicate the cows. 
+#
+# Please note that both secret number and friend's guess may contain duplicate
+# digits.
+#
+# Example 1:
+#
+#
+# Input: secret = "1807", guess = "7810"
+#
+# Output: "1A3B"
+#
+# Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+#
+# Example 2:
+#
+#
+# Input: secret = "1123", guess = "0111"
+#
+# Output: "1A1B"
+#
+# Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a
+# cow.
+#
+# Note: You may assume that the secret number and your friend's guess only
+# contain digits, and their lengths are always equal.
+#
+
+
+class Solution:
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        dic = dict()
+        for n in secret:
+            if n not in dic:
+                dic[n] = 1
+            else:
+                dic[n] += 1
+        # Count Bulls
+        bulls = 0
+        for i in range(len(guess)):
+            if secret[i] == guess[i]:
+                bulls += 1
+                dic[secret[i]] -= 1
+        # Count Cows
+        cows = 0
+        for i in range(len(guess)):
+            if secret[i] != guess[i] and guess[i] in dic and dic[guess[i]] > 0:
+                cows += 1
+                dic[guess[i]] -= 1
+
+        return "{}A{}B".format(bulls, cows)
