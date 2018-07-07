8f16134c2abde90ddf596fb3d9687be2bc9c6e86
Yuan Cao <caoyuan0816@gmail.com>
2018-07-07 17:55:10 +0800

    402 Greedy****

---
+++ 402.remove-k-digits.python3.py
@@ -0,0 +1,68 @@
+#
+# [402] Remove K Digits
+#
+# https://leetcode.com/problems/remove-k-digits/description/
+#
+# algorithms
+# Medium (25.78%)
+# Total Accepted:    34.1K
+# Total Submissions: 132.1K
+# Testcase Example:  '"1432219"\n3'
+#
+# Given a non-negative integer num represented as a string, remove k digits
+# from the number so that the new number is the smallest possible.
+#
+#
+# Note:
+#
+# The length of num is less than 10002 and will be ≥ k.
+# The given num does not contain any leading zero.
+#
+#
+#
+#
+# Example 1:
+#
+# Input: num = "1432219", k = 3
+# Output: "1219"
+# Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219
+# which is the smallest.
+#
+#
+#
+# Example 2:
+#
+# Input: num = "10200", k = 1
+# Output: "200"
+# Explanation: Remove the leading 1 and the number is 200. Note that the output
+# must not contain leading zeroes.
+#
+#
+#
+# Example 3:
+#
+# Input: num = "10", k = 2
+# Output: "0"
+# Explanation: Remove all the digits from the number and it is left with
+# nothing which is 0.
+#
+
+
+class Solution:
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        if len(num) <= k:
+            return "0"
+        stack, d = [], 0
+        for c in num:
+            while d != k and len(stack) != 0 and int(c) < int(stack[-1]):
+                stack.pop()
+                d += 1
+            stack.append(c)
+        if len(stack) == 0:
+            return "0"
+        return str(int(''.join(stack)))[:len(num)-k]
