dd662172c714204852ee5432231f87db30f4f91c
Yuan Cao <caoyuan0816@gmail.com>
2018-07-12 11:27:13 -0700

    437

---
+++ 437.path-sum-iii.python3.py
@@ -0,0 +1,74 @@
+#
+# [437] Path Sum III
+#
+# https://leetcode.com/problems/path-sum-iii/description/
+#
+# algorithms
+# Easy (40.24%)
+# Total Accepted:    63.5K
+# Total Submissions: 157.8K
+# Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+#
+# You are given a binary tree in which each node contains an integer value.
+#
+# Find the number of paths that sum to a given value.
+#
+# The path does not need to start or end at the root or a leaf, but it must go
+# downwards
+# (traveling only from parent nodes to child nodes).
+#
+# The tree has no more than 1,000 nodes and the values are in the range
+# -1,000,000 to 1,000,000.
+#
+# Example:
+#
+# root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+#
+# ⁠     10
+# ⁠    /  \
+# ⁠   5   -3
+# ⁠  / \    \
+# ⁠ 3   2   11
+# ⁠/ \   \
+# 3  -2   1
+#
+# Return 3. The paths that sum to 8 are:
+#
+# 1.  5 -> 3
+# 2.  5 -> 2 -> 1
+# 3. -3 -> 11
+#
+#
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        self.res = 0
+        self.DFS(root, sum)
+        return self.res
+
+    def DFS(self, root, sum, flag=False):
+        if root is None:
+            return
+        if root.val == sum:
+            self.res += 1
+        if flag:
+            self.DFS(root.left, sum-root.val, True)
+            self.DFS(root.right, sum-root.val, True)
+        else:
+            self.DFS(root.left, sum)
+            self.DFS(root.right, sum)
+            self.DFS(root.left, sum-root.val, True)
+            self.DFS(root.right, sum-root.val, True)
