07a7313b44761805abef4d76fddf41fffe7def33
Yuan Cao <caoyuan0816@gmail.com>
2018-07-26 18:06:10 -0700

    589

---
+++ 589.n-ary-tree-preorder-traversal.py
@@ -0,0 +1,49 @@
+#
+# [775] N-ary Tree Preorder Traversal
+#
+# https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+#
+# algorithms
+# Easy (33.26%)
+# Total Accepted:    1.9K
+# Total Submissions: 4.9K
+# Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+#
+# Given an n-ary tree, return the preorder traversal of its nodes' values.
+#
+#
+# For example, given a 3-ary tree:
+#
+#
+#
+#
+# Return its preorder traversal as: [1,3,5,6,2,4].
+#
+#
+# Note: Recursive solution is trivial, could you do it iteratively?
+#
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        self.res = []
+        self.helper(root)
+        return self.res
+
+    def helper(self, root):
+        if root is None:
+            return
+        self.res.append(root.val)
+        for chd in root.children:
+            self.helper(chd)
