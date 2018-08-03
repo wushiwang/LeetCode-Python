f5f961e93b0ec60ef2804b9ada4baa8ce4693b0d
Yuan Cao <caoyuan0816@gmail.com>
2018-08-02 17:49:31 -0700

    654

---
+++ 654.maximum-binary-tree.python3.py
@@ -0,0 +1,66 @@
+#
+# [654] Maximum Binary Tree
+#
+# https://leetcode.com/problems/maximum-binary-tree/description/
+#
+# algorithms
+# Medium (70.76%)
+# Total Accepted:    41.6K
+# Total Submissions: 58.8K
+# Testcase Example:  '[3,2,1,6,0,5]'
+#
+#
+# Given an integer array with no duplicates. A maximum tree building on this
+# array is defined as follow:
+#
+# The root is the maximum number in the array.
+# The left subtree is the maximum tree constructed from left part subarray
+# divided by the maximum number.
+# The right subtree is the maximum tree constructed from right part subarray
+# divided by the maximum number.
+#
+#
+#
+#
+# Construct the maximum tree by the given array and output the root node of
+# this tree.
+#
+#
+# Example 1:
+#
+# Input: [3,2,1,6,0,5]
+# Output: return the tree root node representing the following tree:
+#
+# ⁠     6
+# ⁠   /   \
+# ⁠  3     5
+# ⁠   \    /
+# ⁠    2  0
+# ⁠      \
+# ⁠       1
+#
+#
+#
+# Note:
+#
+# The size of the given array will be in the range [1,1000].
+#
+import math
+
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 0:
+            return None
+        maxx, pos = -math.inf, 0
+        for i in range(len(nums)):
+            if nums[i] > maxx:
+                maxx, pos = nums[i], i
+        node = TreeNode(maxx)
+        node.left = self.constructMaximumBinaryTree(nums[:pos])
+        node.right= self.constructMaximumBinaryTree(nums[pos+1:])
+        return node
