06aa38295e1ccae34f50d7f1b267b787b1ac9666
Yuan Cao <caoyuan0816@gmail.com>
2018-08-09 20:58:38 -0700

    710 Random*******

---
+++ 710.random-pick-with-blacklist.python3.py
@@ -0,0 +1,101 @@
+#
+# [894] Random Pick with Blacklist
+#
+# https://leetcode.com/problems/random-pick-with-blacklist/description/
+#
+# algorithms
+# Hard (29.79%)
+# Total Accepted:    1.6K
+# Total Submissions: 5.4K
+# Testcase Example:  '["Solution", "pick", "pick", "pick"]\n[[1, []], [], [], []]'
+#
+# Given a blacklist B containing unique integers from [0, N), write a function
+# to return a uniform random integer from [0, N) which is NOT in B.
+#
+# Optimize it such that it minimizes the call to system’s Math.random().
+#
+# Note:
+#
+#
+# 1 <= N <= 1000000000
+# 0 <= B.length < min(100000, N)
+# [0, N) does NOT include N. See interval notation.
+#
+#
+# Example 1:
+#
+#
+# Input:
+# ["Solution","pick","pick","pick"]
+# [[1,[]],[],[],[]]
+# Output: [null,0,0,0]
+#
+#
+# Example 2:
+#
+#
+# Input:
+# ["Solution","pick","pick","pick"]
+# [[2,[]],[],[],[]]
+# Output: [null,1,1,1]
+#
+#
+# Example 3:
+#
+#
+# Input:
+# ["Solution","pick","pick","pick"]
+# [[3,[1]],[],[],[]]
+# Output: [null,0,0,2]
+#
+#
+# Example 4:
+#
+#
+# Input:
+# ["Solution","pick","pick","pick"]
+# [[4,[2]],[],[],[]]
+# Output: [null,1,3,1]
+#
+#
+# Explanation of Input Syntax:
+#
+# The input is two lists: the subroutines called and their arguments.
+# Solution's constructor has two arguments, N and the blacklist B. pick has no
+# arguments. Arguments are always wrapped with a list, even if there aren't
+# any.
+#
+import random
+
+
+class Solution:
+
+    def __init__(self, N, blacklist):
+        """
+        :type N: int
+        :type blacklist: List[int]
+        """
+        self.M = N - len(blacklist)
+        self.black = set(blacklist)
+        self.dic = dict()
+        self.pos = self.M
+
+    def pick(self):
+        """
+        :rtype: int
+        """
+        cur = random.randint(0, self.M-1)
+        if cur in self.black:
+            if cur not in self.dic:
+                while self.pos in self.black:
+                    self.pos += 1
+                self.dic[cur] = self.pos
+                self.pos += 1
+            return self.dic[cur]
+        else:
+            return cur
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(N, blacklist)
+# param_1 = obj.pick()
